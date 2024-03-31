a899fe86ab55c58f25cb8beba87cf5d4371fd17f
Zizky <zikar.nurizky@gmail.com>
2024-03-31 22:09:23 +0700

    Refactor code for improved performance and readabi lity. Plus add a new MealItem component to display

---
+++ MealsApp/components/MealItem.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default MealItem = ({ title }) => {
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    );
+}
--- MealsApp/screens/MealsOverviewScreen.js
+++ MealsApp/screens/MealsOverviewScreen.js
@@ -1,10 +1,25 @@
+import MealItem from '../components/MealItem';
 import { MEALS } from '../data/dummy-data';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
-export default MealsOverviewScreen = () => {
+export default MealsOverviewScreen = ({ route }) => {
+    const catId = route.params.categoryId;
+
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
+    const renderMealItem = (itemData) => {
+        return (
+            <MealItem title={itemData.item.title} />
+        );
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen</Text>
+            <FlatList 
+            data={displayedMeals}
+            keyExtractor={item => item.id}
+            renderItem = {renderMealItem}
+            />
         </View>
     );
 }

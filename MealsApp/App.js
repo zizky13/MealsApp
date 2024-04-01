ea761f84876a2b0b7c55b46c510d937f26c08bdb
Zizky <zikar.nurizky@gmail.com>
2024-04-01 16:00:55 +0700

    Refactor font imports and update navigation styling

--- MealsApp/App.js
+++ MealsApp/App.js
@@ -1,32 +1,50 @@
-import { StatusBar, StyleSheet, Text,  } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import { useFonts } from 'expo-font';
+import { StatusBar, StyleSheet, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import { useFonts } from "expo-font";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [loaded] = useFonts({
-    'poppins-reg': require('./assets/fonts/Poppins-Regular.ttf'),
-    'poppins-semi-bold': require('./assets/fonts/Poppins-SemiBold.ttf'),
-    'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
-    'poppins-italic': require('./assets/fonts/Poppins-Italic.ttf'),
-
+    "poppins-reg": require("./assets/fonts/Poppins-Regular.ttf"),
+    "poppins-semi-bold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "poppins-bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "poppins-italic": require("./assets/fonts/Poppins-Italic.ttf"),
   });
 
   if (!loaded) {
     return null;
   }
-  
+
   return (
     <>
       <StatusBar barStyle="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        <Stack.Navigator screenOptions={{
+          headerStyle: {
+            backgroundColor: "#351401",
+          },
+          headerTintColor: "#fff",
+          contentStyle: {
+            backgroundColor: "#222831",
+          },
+        }}>
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "Categories",
+              
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={{}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
